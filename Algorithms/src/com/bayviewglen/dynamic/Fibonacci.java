b7d34fb437de691a708e45fa30b1134a2b5a9a59
Aadithya Shankar <ashankar@bayviewglen.ca>
2016-11-29 13:04:45 -0500

    algorithms unit

---
+++ Algorithms/src/com/bayviewglen/dynamic/Fibonacci.java
@@ -0,0 +1,28 @@
+package com.bayviewglen.dynamic;
+
+public class Fibonacci {
+	static long[] solutions;
+
+	public static void main(String[] args) {
+		int n = 25;
+		solutions = new long[n + 1];
+
+		for (int i = 0; i <= n; ++i) {
+			solutions[i] = -1;
+		}
+		System.out.println(solveFib(n));
+	}
+
+	private static long solveFib(int n) {
+		if (solutions[n] != -1) {
+			return solutions[n];
+		}
+
+		if (n == 1 || n == 2) {
+			return 1;
+		} else {
+			solutions[n] = solveFib(n - 1) + solveFib(n - 2);
+			return solutions[n];
+		}
+	}
+}
